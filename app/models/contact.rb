2d4436ce0417dfe312832a62e179c5de281db2d6
Apprentice <apprentice@dbc07.local>
2013-07-11 09:00:07 -0700

    updated contact and mirgrate

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,21 @@ require_relative '../../db/config'
 
 class Contact< ActiveRecord::Base
 
-# implement your Contact model here
+  attr_reader :age, :areacode
 
+  validates_uniqueness_of :email
+  validates :email, presence: true, format: { with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i} 
+  validates_length_of :phone, :in => 9..32
+  validates_presence_of :company
+
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def areacode
+    phone.slice(0..2)
+  end
+
+  
 end
--- db/migrate/20121011144238_create_contacts.rb
+++ db/migrate/20121011144238_create_contacts.rb
@@ -4,6 +4,14 @@ require_relative '../config'
 
 class CreateContacts < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :company
+      t.string :phone
+      t.string :email
+    end
   end
+
+
 end
